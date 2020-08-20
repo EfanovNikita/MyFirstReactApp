e27216a1aea46261d2e2a7c0e35cb018f9d86024
Efanov Nikita <Efanov_nikitka@mail.ru>
2020-08-20 13:17:13 +0700

    start

---
+++ src/Components/Leftbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Navbar.module.css';
+
+const Navbar = (props) => {
+    return (
+        <div className={style.nav}>
+            <div>A</div>
+            <div>B</div>
+            <div>C</div>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/Components/Rightbar/Rightbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Rightbar.module.css';
+
+const Rightbar = (props) => {
+    return (
+        <div className={style.rightbar}>
+            
+        </div>
+    )
+}
+
+export default Rightbar;
